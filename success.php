371b260591b1bd079ea7ec96548d651f440504df
MadHatJester <genilegame29@gmail.com>
2019-11-09 11:15:20 +0800

    Update success.php

--- success.php
+++ success.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 include "header.php";
 ?>
@@ -14,41 +13,13 @@ include "header.php";
 <h1>Reservation Successful!</h2>
 <p>Your transaction no.: <?php echo $_GET['transacno'] ?></p>
 
-<form action="occupant_index.php">
+<form action="occupant_homepage.php">
     <button class="btn btn-success" type="submit">Home</button>
 </form>
 
 </div>
-
-<?php
-require "footer.php";
-?>
-
-=======
-<?php
-include "header.php";
-?>
-
-<html>
-    <head>
-        <meta charset="utf-8">
-		<link rel="stylesheet" href="css/bootstrap/css/bootstrap.css">
-        <title>WaterBNB - Reservation Success!</title>
-    </head>
-
-<div class="jumbotron p-4 m-4">
-<h1>Reservation Successful!</h2>
-<p>Your transaction no.: <?php echo $_GET['transacno'] ?></p>
-
-<form action="occupant_index.php">
-    <button class="btn btn-success" type="submit">Home</button>
-</form>
-
-</div>
-
-<?php
-require "footer.php";
-?>
-
->>>>>>> c2695829c93f97271b41305c0fd3e6b4cbd9e8e8
 </html>
+
+<?php
+require "footer.php";
+?>
